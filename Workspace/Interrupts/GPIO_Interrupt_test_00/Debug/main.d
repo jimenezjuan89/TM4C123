b77ce6a26d8db12441d5f7e9c7b3134947095bfc
Juan Jimenez <jimenezjuan89@gmail.com>
2019-04-04 19:04:22 -0500

    started an interrupts project

---
+++ Workspace/Interrupts/GPIO_Interrupt_test_00/Debug/main.d
@@ -0,0 +1,22 @@
+main.o: ../main.c \
+ C:/Users/jimen/OneDrive/Documents/Workspace/CodeComposerStudio/gitWorkspace/TM4C123/CMSIS/TI_CMSIS/TM4C123GH6PM.h \
+ C:/Users/jimen/OneDrive/Documents/Workspace/CodeComposerStudio/gitWorkspace/TM4C123/CMSIS/ARM_CMSIS/core_cm4.h \
+ C:/Users/jimen/OneDrive/Documents/Workspace/CodeComposerStudio/gitWorkspace/TM4C123/CMSIS/ARM_CMSIS/cmsis_version.h \
+ C:/Users/jimen/OneDrive/Documents/Workspace/CodeComposerStudio/gitWorkspace/TM4C123/CMSIS/ARM_CMSIS/cmsis_compiler.h \
+ C:/Users/jimen/OneDrive/Documents/Workspace/CodeComposerStudio/gitWorkspace/TM4C123/CMSIS/ARM_CMSIS/cmsis_gcc.h \
+ C:/Users/jimen/OneDrive/Documents/Workspace/CodeComposerStudio/gitWorkspace/TM4C123/CMSIS/ARM_CMSIS/mpu_armv7.h \
+ C:/Users/jimen/OneDrive/Documents/Workspace/CodeComposerStudio/gitWorkspace/TM4C123/CMSIS/TI_CMSIS/system_TM4C.h
+
+C:/Users/jimen/OneDrive/Documents/Workspace/CodeComposerStudio/gitWorkspace/TM4C123/CMSIS/TI_CMSIS/TM4C123GH6PM.h:
+
+C:/Users/jimen/OneDrive/Documents/Workspace/CodeComposerStudio/gitWorkspace/TM4C123/CMSIS/ARM_CMSIS/core_cm4.h:
+
+C:/Users/jimen/OneDrive/Documents/Workspace/CodeComposerStudio/gitWorkspace/TM4C123/CMSIS/ARM_CMSIS/cmsis_version.h:
+
+C:/Users/jimen/OneDrive/Documents/Workspace/CodeComposerStudio/gitWorkspace/TM4C123/CMSIS/ARM_CMSIS/cmsis_compiler.h:
+
+C:/Users/jimen/OneDrive/Documents/Workspace/CodeComposerStudio/gitWorkspace/TM4C123/CMSIS/ARM_CMSIS/cmsis_gcc.h:
+
+C:/Users/jimen/OneDrive/Documents/Workspace/CodeComposerStudio/gitWorkspace/TM4C123/CMSIS/ARM_CMSIS/mpu_armv7.h:
+
+C:/Users/jimen/OneDrive/Documents/Workspace/CodeComposerStudio/gitWorkspace/TM4C123/CMSIS/TI_CMSIS/system_TM4C.h:
---
+++ Workspace/Interrupts/GPIO_Interrupt_test_00/main.c
@@ -0,0 +1,47 @@
+//test_00
+//GPIO Interrupt
+
+/**
+ * main.c
+ */
+#include "TM4C123GH6PM.h"
+
+#define ledRed      (1u<<1)
+#define ledBlue     (1u<<2)
+#define ledGreen    (1u<<3)
+#define SW1         (1u<<4)
+#define SW2         (1u<<0)
+
+#define unlock      0x4C4F434B
+
+void GPIOF_init(void);
+
+int main(void)
+{
+    GPIOF_init();
+
+    while(1);
+
+	return 0;
+}
+
+void GPIOF_init(void) {
+    SYSCTL->RCGCGPIO |= (1U<<5);
+    SYSCTL->GPIOHBCTL |= (1U<<5);
+    GPIOF_AHB->LOCK = unlock;
+//    GPIOF_AHB->CR = (ledRed | ledBlue | ledGreen | SW1 | SW2);
+    GPIOF_AHB->DEN |= (ledRed | ledBlue | ledGreen | SW1 | SW2);
+    GPIOF_AHB->DIR |= (ledRed | ledBlue | ledGreen);
+    GPIOF_AHB->DIR &= ~(SW1 | SW2);
+    GPIOF_AHB->PUR |= (SW1 | SW2);          //enable weak pull-up resistor
+    GPIOF_AHB->IS &= ~(SW1 | SW2);          //make SWs edge sensitive
+    GPIOF_AHB->IBE &= ~(SW1 | SW2);         //make this register in conjuction with IEV
+    GPIOF_AHB->IEV &= ~(SW1 | SW2);         //make SWs rising edge triggered
+    GPIOF_AHB->ICR |= (SW1 | SW2);          //clear previous interrupts
+    GPIOF_AHB->IM |= (SW1 | SW2);           //unmask the interrupt for SWs
+    NVIC->IP[30] = 0X00600000;              //assign priority as 3
+    NVIC->ISER[0] |= (1U<<30);              //enable interrupt for IRQ30
+    __enable_irq();
+
+}
+
